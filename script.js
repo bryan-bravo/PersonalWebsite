a78d1a7d539a0f63ac972a3220e7a8b7deff9f1f
[bryan-bravo] <[bryanbravo90@yahoo.com]>
2017-12-15 15:29:28 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+$(function() {
+    //start
+    $("#resume").hide();
+    $("#portfolio").hide();
+    $("#left-arrow").hide();
+    $("#right-arrow").hide();
+
+    //home to resume
+    $("#resume-link").click(function() {
+        $("#home-page").hide("slide", { direction: "right" }, function() {
+            $("#resume").show("slide", { direction: "left" });
+            $("#right-arrow").show();
+        });
+    });
+    //home to portfolio
+    $("#portfolio-link").click(function() {
+        $("#home-page").hide("slide", { direction: "left" }, function() {
+            $("#right-arrow").hide();
+            $("#portfolio").show("slide", { direction: "right" });
+            $("#left-arrow").show();
+        });
+    });
+    //resume to home
+    $("#right-arrow").click(function() {
+        $("#resume").hide("slide", { direction: "left" }, function() {
+            $("#home-page").show("slide", { direction: "right" });
+            $("#right-arrow").hide();
+        });
+    });
+
+    //portfolio to home
+    $("#left-arrow").click(function() {
+        $("#portfolio").hide("slide", { direction: "right" }, function() {
+            $("#home-page").show("slide", { direction: "left" });
+            $("#left-arrow").hide();
+
+        });
+    });
+});
