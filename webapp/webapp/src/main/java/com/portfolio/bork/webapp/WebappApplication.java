8db03670ddba42a637e53cffbafbaf55fece5ea0
bryan bravo <bryanbravo90@yahoo.com>
2020-03-12 20:44:14 -0700

    This commit has the base spring bologne

---
+++ webapp/webapp/src/main/java/com/portfolio/bork/webapp/WebappApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.bork.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappApplication.class, args);
+	}
+
+}
