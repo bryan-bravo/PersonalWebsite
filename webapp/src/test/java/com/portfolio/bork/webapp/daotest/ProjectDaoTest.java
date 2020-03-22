5b92dcd78c3818617623adefe73b30991bfd2fee
bryan bravo <bryanbravo90@yahoo.com>
2020-03-21 21:24:08 -0700

    This commit got the test working c:

--- webapp/src/test/java/com/portfolio/bork/webapp/daotest/ProjectDaoTest.java
+++ webapp/src/test/java/com/portfolio/bork/webapp/daotest/ProjectDaoTest.java
@@ -1,5 +1,21 @@
 package com.portfolio.bork.webapp.daotest;
 
-public class ProjectDaoTest {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.testng.annotations.Test;
+import com.portfolio.bork.webapp.services.db.ProjectDao;
+
+@Test
+@ContextConfiguration(locations = "classpath:SpringTestContext.xml")
+public class ProjectDaoTest extends AbstractTransactionalTestNGSpringContextTests {
     
+    @Autowired
+    ProjectDao projectDao;
+
+    @Test(description = "Get Project")
+	public void getProject() {
+
+        assert projectDao.getProjectById(1L) != null;
+	}
 }
